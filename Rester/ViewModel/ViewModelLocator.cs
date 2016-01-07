a03dc2ec2d97d4e80d375fa117186f2e851aa0fb
johan@classon.eu <johan@classon.eu>
2016-01-07 19:50:16 +0100

    Project base

---
+++ Rester/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,46 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Rester.ViewModel
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<INavigationService, NavigationService>();
+            SimpleIoc.Default.Register<IDialogService, DialogService>();
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                SimpleIoc.Default.Register<IDeviceStore, DesignDeviceStore>();
+            }
+            else
+            {
+                SimpleIoc.Default.Register<IDeviceStore, DeviceStore>();
+            }
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+    }
+
+    internal class DeviceStore : IDeviceStore
+    {
+    }
+
+    internal class DesignDeviceStore : IDeviceStore
+    {
+    }
+
+    public interface IDeviceStore
+    {
+    }
+}
