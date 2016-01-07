4599d7cf87d8d978b531de65d7ab0b1a2f99bb63
johan@classon.eu <johan@classon.eu>
2016-01-07 19:50:16 +0100

    Project base

---
+++ Rester/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Rester.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDeviceStore _deviceStore;
+
+        public MainViewModel(IDeviceStore deviceStore)
+        {
+            _deviceStore = deviceStore;
+        }
+
+        private string _text = "Some dummy text";
+        public string Text
+        {
+            get { return _text; }
+            set { Set(nameof(Text), ref _text, value); }
+        }
+    }
+}
