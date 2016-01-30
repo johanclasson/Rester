fd35d23c8043a45ed66773b18d2d7e449095bc35
johan@classon.eu <johan@classon.eu>
2016-01-30 19:30:45 +0100

    Converters for edit mode and red/green rest methods

--- Rester/ViewModel/MainViewModel.cs
+++ Rester/ViewModel/MainViewModel.cs
@@ -17,6 +17,8 @@ namespace Rester.ViewModel
             _navigationService = navigationService; //For some reason, the navigation does not work if not kept as a member
             LoadData();
             NavigateToLogCommand = new RelayCommand(() => _navigationService.NavigateTo(LogPage.Key));
+            EditModeCommand = new RelayCommand(() => EditMode = true);
+            EditCompletedCommand = new RelayCommand(() => EditMode = false); //TODO: Save to store now or on every change?
         }
 
         private async void LoadData()
@@ -27,6 +29,11 @@ namespace Rester.ViewModel
 
         public ObservableCollection2<ServiceConfiguration> ServiceConfigurations { get; } = new ObservableCollection2<ServiceConfiguration>();
 
+        public bool EditMode { get { return _editMode; } set { Set(nameof(EditMode), ref _editMode, value); } }
+        private bool _editMode;
+
         public ICommand NavigateToLogCommand { get; }
+        public ICommand EditModeCommand { get; }
+        public ICommand EditCompletedCommand { get; }
     }
 }
