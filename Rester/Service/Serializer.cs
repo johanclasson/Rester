1e4b59ea328923360e237dcbe8dda56f2a05106f
johan@classon.eu <johan@classon.eu>
2016-02-03 18:43:24 +0100

    Zipper, and serialization tests

--- Rester/Service/Serializer.cs
+++ Rester/Service/Serializer.cs
@@ -3,7 +3,7 @@ using Rester.Model;
 
 namespace Rester.Service
 {
-    internal interface ISerializer
+    public interface ISerializer
     {
         string Serialize(ServiceConfiguration[] configurations);
     }
@@ -15,38 +15,56 @@ namespace Rester.Service
             var jConfigArray = new JArray();
             foreach (ServiceConfiguration configuration in configurations)
             {
-                var jEndpointArray = new JArray();
-                var jConfig = new JObject
-                {
-                    ["Name"] = configuration.Name,
-                    ["BaseUri"] = configuration.BaseUri,
-                    ["Endpoints"] = jEndpointArray
-                };
-                foreach (ServiceEndpoint endpoint in configuration.Endpoints)
-                {
-                    var jActionArray = new JArray();
-                    var jEndpoint = new JObject
-                    {
-                        ["Name"] = endpoint.Name,
-                        ["Actions"] = jActionArray
-                    };
-                    foreach (ServiceEndpointAction action in endpoint.Actions)
-                    {
-                        var jAction = new JObject()
-                        {
-                            ["Name"] = action.Name,
-                            ["Body"] = action.Body,
-                            ["MediaType"] = action.MediaType,
-                            ["Method"] = action.Method,
-                            ["UriPath"] = action.UriPath
-                        };
-                        jActionArray.Add(jAction);
-                    }
-                    jEndpointArray.Add(jEndpoint);
-                }
+                JObject jConfig = SerializeServiceConfiguraion(configuration);
                 jConfigArray.Add(jConfig);
             }
             return jConfigArray.ToString();
+        }
+
+        private static JObject SerializeServiceConfiguraion(ServiceConfiguration configuration)
+        {
+            var jEndpointArray = new JArray();
+            var jConfig = new JObject
+            {
+                ["Name"] = configuration.Name,
+                ["BaseUri"] = configuration.BaseUri,
+                ["Endpoints"] = jEndpointArray
+            };
+            foreach (ServiceEndpoint endpoint in configuration.Endpoints)
+            {
+                JObject jEndpoint = SerializeEndpoint(endpoint);
+                jEndpointArray.Add(jEndpoint);
+            }
+            return jConfig;
+        }
+
+        private static JObject SerializeEndpoint(ServiceEndpoint endpoint)
+        {
+            var jActionArray = new JArray();
+            var jEndpoint = new JObject
+            {
+                ["Name"] = endpoint.Name,
+                ["Actions"] = jActionArray
+            };
+            foreach (ServiceEndpointAction action in endpoint.Actions)
+            {
+                JObject jAction = SerializeAction(action);
+                jActionArray.Add(jAction);
+            }
+            return jEndpoint;
+        }
+
+        private static JObject SerializeAction(ServiceEndpointAction action)
+        {
+            var jAction = new JObject()
+            {
+                ["Name"] = action.Name,
+                ["Body"] = action.Body,
+                ["MediaType"] = action.MediaType,
+                ["Method"] = action.Method,
+                ["UriPath"] = action.UriPath
+            };
+            return jAction;
         }
     }
 }
